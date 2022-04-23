9914000fc9908f4d0f027d6a6d3abb1262900030
izemlyanukhin <vanyok-z07@yandex.ru>
2022-04-23 15:17:43 +0300

    #1-4 - checkSpam function

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,17 @@
 function checkSpam(str) {
-  // ваш код...
+  
+  const stoplist = [
+    '1xbet', 
+    'xxx',
+  ];
+  
+  str = str.toLowerCase();
+  
+  for (let i = 0; i < stoplist.length; i++) {
+  	if (str.includes(stoplist[i])) {
+      return true;    
+    }
+  };
+  
+  return false;
 }
