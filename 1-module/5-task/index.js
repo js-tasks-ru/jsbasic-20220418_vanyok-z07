2ff1c72565dadd7a7c19f85d4fd94aa5a2e4cccb
izemlyanukhin <vanyok-z07@yandex.ru>
2022-04-23 15:35:28 +0300

    #1-5 - truncate function

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,11 @@
 function truncate(str, maxlength) {
-  // ваш код...
+
+  let result = str;
+  const suffix = '…';
+
+  if (str.length > --maxlength) {
+    result = str.slice(0, maxlength) + suffix;
+  }
+
+  return result;
 }
