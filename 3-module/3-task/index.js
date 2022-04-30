3d033c8330aee3a0a8f8d49cf157a4eb931e841b
izemlyanukhin <vanyok-z07@yandex.ru>
2022-04-30 18:03:22 +0300

    #3-3 - camelize function

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,12 @@
 function camelize(str) {
-  // ваш код...
+  return str
+    .split('-')
+    .map((item, i) => {
+      let res = item;
+      if (i > 0) {
+        res = item[0].toUpperCase() + item.slice(1);
+      }
+      return res;
+    })
+    .join('');
 }
