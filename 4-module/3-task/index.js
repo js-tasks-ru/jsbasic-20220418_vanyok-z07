574a4a953ec79bac824a1c34ff0a49c018cb9474
izemlyanukhin <vanyok-z07@yandex.ru>
2022-05-12 11:33:41 +0300

    #4-3 - highlight function

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,24 @@
 function highlight(table) {
-  // ваш код...
+
+  const rows = Array.from(table.rows).splice(1, table.rows.length);
+
+  for (let row of rows) {
+    const availableStatus = row.cells[3].dataset.available;
+    const availableClass = availableStatus === 'true' ? 'available' : 'unavailable';
+    const genderClass = row.cells[2].innerText === 'm' ? 'male' : 'female';
+
+    const isUnderage = row.cells[1].innerText < 18;
+    const underageStyle = `text-decoration: line-through;`;
+
+    row.classList.add(genderClass);
+    row.classList.add(availableClass);
+
+    if (typeof availableStatus === 'undefined') {
+      row.setAttribute('hidden', true);
+    }
+
+    if (isUnderage) {
+      row.style.cssText = underageStyle;
+    }
+  }
 }
