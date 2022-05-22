ada4c5a5664c64aa0c37ad4fcd46566d18e25edf
izemlyanukhin <vanyok-z07@yandex.ru>
2022-05-22 23:12:57 +0300

    HW #5 - hideSelf, toggleText and initCarousel

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,14 @@
 function toggleText() {
-  // ваш код...
+  const btn = document.querySelector('.toggle-text-button');
+  const clickHandler = function () {
+    const text = document.querySelector('#text');
+
+    if (text) {
+      text.hidden = !text.hidden;
+    }
+  };
+
+  if (btn) {
+    btn.addEventListener('click', clickHandler);
+  }
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,41 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector('.carousel');
+  const carouselInner = carousel.querySelector('.carousel__inner');
+  const controls = carousel.querySelectorAll('[class*="carousel__arrow_"]');
+  const nextControl = controls[0];
+  const prevControl = controls[1];
+  const maxSlideNumber = carouselInner.childElementCount - 1;
+  const slideWidth = carouselInner.clientWidth;
+
+  let currentSlide = 0;
+
+  const setControlsVisibility = function () {
+    if (currentSlide === 0) {
+      prevControl.style.display = 'none';
+      nextControl.style.display = '';
+    } else if (currentSlide < maxSlideNumber) {
+      prevControl.style.display = '';
+      nextControl.style.display = '';
+    } else if (currentSlide === maxSlideNumber) {
+      nextControl.style.display = 'none';
+      prevControl.style.display = '';
+    }
+  };
+
+  const controlClickHandler = function (e) {
+    const dir = this.classList.contains('carousel__arrow_right') ? 1 : -1;
+
+    currentSlide += dir;
+
+    carouselInner.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+
+    setControlsVisibility();
+
+  };
+
+  Array.from(controls).forEach(control => {
+    control.addEventListener('click', controlClickHandler);
+  });
+
+  setControlsVisibility();
 }
