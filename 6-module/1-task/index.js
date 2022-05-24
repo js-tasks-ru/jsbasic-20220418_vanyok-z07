4842ac31ed0bda1eeb99ee9b34d5e7c17c4ac9b0
izemlyanukhin <vanyok-z07@yandex.ru>
2022-05-24 23:47:10 +0300

    HW #6 - UserTable, ProductCard, Carousel

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,70 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.titles = {
+      name: 'Имя',
+      age: 'Возраст',
+      salary: 'Зарплата',
+      city: 'Город',
+    };
+    this.rows = rows;
+    this._elem = document.createElement('table');
+
+    this._elem.addEventListener('click', this.removeHandler);
+
+    this.render();
+  }
+
+  get elem() {
+    return this._elem;
+  }
+
+  render() {
+    this.elem.appendChild(this.renderHead());
+    this.elem.appendChild(this.renderBody());
+    return this.elem;
+  }
+
+  renderHead() {
+    const thead = document.createElement('thead');
+
+    let headString = '<tr>';
+
+    for (let key in this.rows[0]) {
+      headString += `<th>${this.titles[key]}</th>`
+    }
+    headString += '<th></th></tr>';
+
+    thead.innerHTML = headString;
+    return thead;
+  }
+
+  renderBody() {
+    const body = document.createElement('tbody');
+
+    let rows = this.rows.map(row => {
+
+      let rowString = '<tr>';
+
+      for (let val in row) {
+        rowString += `<td>${row[val]}</td>`;
+      }
+      rowString += `<td><button>&#11199;</button></td></tr>`;
+
+      return rowString;
+    });
+
+    body.innerHTML = rows.join('');
+    return body;
+  }
+
+  removeHandler(e) {
+    if (e.target.closest('button')) {
+      const row = e.target.closest('tr');
+
+      if (row) {
+        row.remove();
+      }
+    }
   }
 }
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,53 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
   constructor(product) {
+    this.product = product;
+    this._elem = null;
+    this.render();
+    this.elem.addEventListener('click', this.addToCart.bind(this));
+  }
+
+  get price() {
+    return `€${this.product.price.toFixed(2)}`;
+  }
+
+  get imgPath() {
+    return `/assets/images/products/${this.product.image}`;
+  }
+
+  get name() {
+    return this.product.name;
+  }
+
+  get elem() {
+    return this._elem;
+  }
+
+  render() {
+    const tpl =
+      `<div class='card'>
+        <div class='card__top'>
+            <img src='${this.imgPath}' class='card__image' alt='product'>
+            <span class='card__price'>${this.price}</span>
+        </div>
+        <div class='card__body'>
+            <div class='card__title'>${this.name}</div>
+            <button type='button' class='card__button'>
+                <img src='/assets/images/icons/plus-icon.svg' alt='icon'>
+            </button>
+        </div>
+    </div>`;
+    this._elem = createElement(tpl);
+  }
+
+  addToCart(e) {
+    if (e.target.closest('.card__button')) {
+      const event = new CustomEvent('product-add', {
+        detail: this.product.id,
+        bubbles: true,
+      });
+      this.elem.dispatchEvent(event);
+    }
   }
 }
